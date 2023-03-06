eebd01d46ca2691bf13d81b95a46b58986817ed8
dhaneshwar10 <125950387+dhaneshwar10@users.noreply.github.com>
2023-03-06 21:35:13 +0530

    Add files via upload

---
+++ PROJECT5-main/src/main/java/com/example/demo/Portal5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portal5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portal5Application.class, args);
+	}
+
+}
